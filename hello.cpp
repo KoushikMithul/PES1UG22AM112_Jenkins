02775067d70fb4c0be92694d5367d00bb3f236db
Koushik Mithul <koushikmithul123@gmail.com>
2025-03-13 16:16:08 +0530

    added hellp.cpp

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "I have made changes in the hello.cpp file \n";
+  cout << "Pipeline is working fine \n";
+  return 0;
+}
